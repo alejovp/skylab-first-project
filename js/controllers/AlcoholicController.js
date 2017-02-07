8c06eccafe5b4c6101bb1f43cd397131753a0468
Alejandro Vazquez <alejovp@gmail.com>
2017-02-07 16:19:07 +0100

    Controllers clean up

--- js/controllers/AlcoholicController.js
+++ js/controllers/AlcoholicController.js
@@ -1,33 +1,25 @@
   angular.module('myCocktailApp')
 
-
   .controller('AlcoholicDrinks', function ($scope, $location, CocktailFactory) {
     CocktailFactory.getAlcoholicDrinks()
       .then(function (response) {
-        // $scope.alcoholicDrinks = response.data.drinks
-       // console.log(response.data.drinks)
         $scope.drinksList = response.data.drinks
       })
 
-    $scope.doSomething = function (id) {
+    $scope.goToDetails = function (id) {
       console.log('Current ID Cocktail = ' + id)
       $location.path('/drink/' + id)
-      // CocktailFactory.getCocktailIngredients(id)
-      //   .then( function(response) {} )
     }
   })
 
    .controller('NoAlcoholicsDrinks', function ($scope, CocktailFactory) {
-    CocktailFactory.getNoAlcoholicDrinks()
+     CocktailFactory.getNoAlcoholicDrinks()
       .then(function (response) {
-        // $scope.noAlcoholicDrinks = response.data.drinks
-        // console.log(noAlcoholicDrinks)
         $scope.drinksList = response.data.drinks
       })
-       $scope.doSomething = function (id) {
-      console.log('Current ID Cocktail = ' + id)
-      $location.path('/drink/' + id)
-      // CocktailFactory.getCocktailIngredients(id)
-      //   .then( function(response) {} )
-    }
-  })
+
+     $scope.goToDetails = function (id) {
+       console.log('Current ID Cocktail = ' + id)
+       $location.path('/drink/' + id)
+     }
+   })
--- js/controllers/DetailController.js
+++ js/controllers/DetailController.js
@@ -1,15 +1,14 @@
 angular.module('myCocktailApp')
 
-.controller('DetailController', function($scope, DetailFactory, $routeParams) {
+.controller('DetailController', function ($scope, DetailFactory, $routeParams) {
+  $scope.getDetail = function (e) {
+    e.preventDefault()
+    DetailFactory.getDetail($routeParams.id)
+            .then(function (response) {
+              $scope.cocktail = response.data.drinks
 
-    $scope.getDetail = function(e) {
-        e.preventDefault()
-        DetailFactory.getDetail($routeParams.id)
-            .then(function(response) {
-                $scope.cocktail = response.data.drinks
-                console.log($routeParams.id)
-                console.log(response.data.drinks)
+              console.log($routeParams.id)
+              console.log(response.data.drinks)
             })
-    }
-
+  }
 })
