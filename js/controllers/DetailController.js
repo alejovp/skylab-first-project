9248dc5a25e3d41d670ba7991d288f3af04e4372
Alejandro Vazquez <alejovp@gmail.com>
2017-02-08 11:04:08 +0100

    Refactor detail controller

--- js/controllers/DetailController.js
+++ js/controllers/DetailController.js
@@ -1,11 +1,19 @@
 angular.module('myCocktailApp')
 
-.controller('DetailController', function($scope, DetailFactory, $routeParams) {
-
-    DetailFactory.getDetail($routeParams.id)
-        .then(function(response) {
-            $scope.cocktail = response.data.drinks
-            console.log($routeParams.id)
-            console.log(response.data.drinks)
+.controller('DetailController', function ($scope, DetailFactory, $routeParams) {
+  DetailFactory.getDetail($routeParams.id)
+        .then(function (response) {
+          $scope.cocktail = response.data.drinks
+          console.log($routeParams.id)
+          console.log(response.data.drinks)
         })
+
+  $scope.getIngredients = function (obj) {
+    var ingArray = []
+    for (var i = 1; i < 15; i++) {
+      ingArray.push(obj['strIngredient' + i])
+    }
+    console.log(ingArray)
+    return ingArray
+  }
 })
