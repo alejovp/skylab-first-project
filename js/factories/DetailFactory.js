9b90bca635d31cdce1a23cdfb5ec66ae19858068
Xavier Meroño <x.merono@gmail.com>
2017-02-10 10:38:56 +0100

    comments and console.logs Deleted

--- js/factories/DetailFactory.js
+++ js/factories/DetailFactory.js
@@ -1,15 +1,15 @@
-(function () {
-  angular.module('myCocktailApp')
+(function() {
+    angular.module('myCocktailApp')
 
-  .factory('DetailFactory', function ($http) {
-    function getDetail (cocktailId) {
-      var url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=<%COCKTAIL-ID%>'
-      url = url.replace('<%COCKTAIL-ID%>', cocktailId)
-      return $http.get(url)
-    }
+    .factory('DetailFactory', function($http) {
+        function getDetail(cocktailId) {
+            var url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=<%COCKTAIL-ID%>'
+            url = url.replace('<%COCKTAIL-ID%>', cocktailId)
+            return $http.get(url)
+        }
 
-    return {
-      getDetail: getDetail
-    }
-  })
+        return {
+            getDetail: getDetail
+        }
+    })
 })()
--- js/factories/IngredientFactory.js
+++ js/factories/IngredientFactory.js
@@ -1,16 +1,15 @@
-(function () {
-  angular.module('myCocktailApp')
+(function() {
+    angular.module('myCocktailApp')
 
-.factory('IngredientFactory', function ($http) {
-  function getByIngredient (ingredient) {
-    // var ingredient = "vodka"
-    var url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=<%INGREDIENT%>'
-    url = url.replace('<%INGREDIENT%>', ingredient)
-    return $http.get(url)
-  }
+    .factory('IngredientFactory', function($http) {
+        function getByIngredient(ingredient) {
+            var url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=<%INGREDIENT%>'
+            url = url.replace('<%INGREDIENT%>', ingredient)
+            return $http.get(url)
+        }
 
-  return {
-    getByIngredient: getByIngredient
-  }
-})
+        return {
+            getByIngredient: getByIngredient
+        }
+    })
 })()
